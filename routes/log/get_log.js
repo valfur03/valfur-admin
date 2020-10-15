3d4f6186fc6ea2dad915953e8358379fa3158b16
Valentin <valentin.furmanek@gmail.com>
2020-10-15 20:54:52 +0200

    Parse the offset to int

--- routes/log/get_log.js
+++ routes/log/get_log.js
@@ -3,7 +3,7 @@ const { get_logs } = require("./query/get_log");
 
 router.get("/all", async (req, res) => {
 	req.body = undefined;
-	const offset = req.query.offset ? req.query.offset : 0;
+	const offset = req.query.offset ? parseInt(req.query.offset) : 0;
 	try {
 		const logs = await get_logs(offset);
 		if (logs.length == 0) return (res.status(404).send({message: "No log found"}));
