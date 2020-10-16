3111f2cd0d95c9f2cc4f58d5b235a31f9e466af7
Valentin <valentin.furmanek@gmail.com>
2020-10-16 15:39:49 +0200

    Add res.send() to warmup route

--- warmup.js
+++ warmup.js
@@ -4,6 +4,7 @@ const connection = require("./modules/db_connect");
 router.get("/", (req, res) => {
 	const query = "SELECT id FROM users";
 	connection.query(query);
+	res.send("Success");
 })
 
 module.exports = router;
