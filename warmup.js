0a9f8d29dd6b49fc817db1388db136d21985f554
Valentin <valentin.furmanek@gmail.com>
2020-10-22 19:17:14 +0200

    Remove logging when warming the server up

--- warmup.js
+++ warmup.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
 const connection = require("./modules/db_connect");
-const data_log = require("./modules/log");
 
 router.get("/", (req, res) => {
 	const query = "SELECT id FROM users";
 	connection.query(query);
-	data_log("Warm up", "Server warmed up, and everything is OK.", "log");
 	res.send("Success");
 })
 
