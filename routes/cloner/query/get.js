16f99b917f91276909e9197302fba4b52fe977e8
Valentin <valentin.furmanek@gmail.com>
2020-11-02 12:32:02 +0100

    Return an empty array if no config was found

--- routes/cloner/query/get.js
+++ routes/cloner/query/get.js
@@ -4,7 +4,7 @@ function get_cloner_config() {
 	return (new Promise((resolve, reject) => {
 		let query = "SELECT url, secret FROM cloner ORDER BY id DESC LIMIT 1";
 		connection.query(query, (error, result) => {
-			return (error ? reject({code: 500, message: "Error when fetching the cloner config"}) : resolve(result[0]));
+			return (error ? reject({code: 500, message: "Error when fetching the cloner config"}) : resolve(result[0] || []));
 		});
 	}));
 }
