de77b63e031af8c0ca59151d168976e0870e441c
Valentin <valentin.furmanek@gmail.com>
2020-11-30 12:39:07 +0100

    Clear error logs after 3 * hours

--- routes/log/query/clear.js
+++ routes/log/query/clear.js
@@ -11,7 +11,7 @@ function clear_all(hours = 24) {
 
 function clear_logs(hours = 24) {
 	return (new Promise((resolve, reject) => {
-		let query = "DELETE FROM log WHERE date_time <= now() - interval ? HOUR AND type = 'log'";
+		let query = "DELETE FROM log WHERE (date_time <= now() - interval ? HOUR AND type = 'log') OR (date_time <= now() - interval ? HOUR AND type = 'error')";
 		connection.query(query, [ hours ], (error, result) => {
 			return (error ? reject({code: 500, message: "Error when deleting logs of type log"}) : resolve(result));
 		});
